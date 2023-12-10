b9504a94044a5a1c44ec257f9231036d5e30c9e3
liu-yinhang <liuyinhang0315@163.com>
2023-12-10 10:46:10 +0800

    icon

--- learn_vue/v3-music/README.md
+++ learn_vue/v3-music/README.md
@@ -32,3 +32,18 @@ devDependencies 开发时的依赖
 - vite.config.js 是 vite 工程化的配置文件
     - alias 短路径
     例如 ："@" -> ./src 类似于回城
+- 企业级vue 开发框架 element-plus PC版
+    好多组件拿来就用
+
+    vue 是渐进式的MVVM 开发框架 开发模式
+    element-plus 组件库 找到我们想要的组件
+
+- vite 写了一段让Element plus 自动引入的代码（省去import）
+
+- 打造通用组件
+    位于 /components/common/
+    不属于特定的组件，有极高的复用性 在多个项目
+    中可以复用
+- 图标字体库
+    要用到哪些图标？
+        import {} from '@icon-park/vue-next'
--- learn_vue/v3-music/src/components/layout/menu/MenuList.vue
+++ learn_vue/v3-music/src/components/layout/menu/MenuList.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <!-- 用户体验的直觉 滚动的列表 详情页-->
+    <ElScrollbar>
         <div class="mt-10 pl-6 pr-6 space-y-1.5" v-for="menuItem in menus">
             <div class="menu-title text-main">{{menuItem.name}}</div>
             <div class="menu-item hover-bg-main text-main"
@@ -8,81 +9,23 @@
                 :class="{'active': currentKey === menu.key}"
                 @click="changeMenu(menu)"
             >
+                <IconPark :icon="menu.icon" size="18" :theme="menu.theme" />
                 <span class="ml-1">{{menu.name}}</span>
             </div>
         </div>
-    </div>
+    </ElScrollbar>
 </template>
 
 <script setup>
-import {ref} from 'vue';
-import {Planet, Music, VideoOne, Fm, Like, Computer, DownloadThree, PlayTwo} from '@icon-park/vue-next'
-let currentKey = ref('discover')
+import IconPark from '@/components/common/IconPark.vue'
 
-const changeMenu = (menu) => {
-    currentKey.value = menu.key
-}
+//         高级写法
+// 模块化（'./useMenu.js'） 封装一个useMenu hooks编程
+// 数据离开组件，组件就很好读
+// 范式 规定用 use 开头 函数式编程
+import { useMenu } from './useMenu.js'
+const { menus, currentKey, changeMenu } = useMenu()
 
-const menus = [
-        {
-            name: "在线音乐",
-            menus: [
-                {
-                    name: "推荐",
-                    key: "discover",
-                    icon: Planet,
-                    theme: 'outline',
-                },
-                {
-                    name: "音乐馆",
-                    key: "music",
-                    icon: Music,
-                    theme: 'outline',
-                },
-                {
-                    name: "视频",
-                    key: "video",
-                    icon: VideoOne,
-                    theme: 'outline',
-                },
-                {
-                    name: "电台",
-                    key: "dj",
-                    icon: Fm,
-                    theme: 'outline',
-                },
-            ]
-        },
-        {
-            name: "我的音乐",
-            menus: [
-                {
-                    name: "我喜欢",
-                    key: "love",
-                    icon: Like,
-                    theme: 'outline',
-                },
-                {
-                    name: "本地歌曲",
-                    key: "local",
-                    icon: Computer,
-                    theme: 'outline',
-                },
-                {
-                    name: "下载歌曲",
-                    key: "download",
-                    icon: DownloadThree,
-                    theme: 'outline',
-                },
-                {
-                    name: "最近播放",
-                    key: "recently",
-                    icon: PlayTwo,
-                    theme: 'outline'
-                },
-            ]
-        }
-    ]
 </script>
 
 <style scoped>
