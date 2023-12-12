07b94866a4736791d896d17af435d232c992f510
liu-yinhang <liuyinhang0315@163.com>
2023-12-12 21:43:19 +0800

    vue-sell

---
+++ learn_vue/vue-sell/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-header :seller="sellerData"/>
+  
+  <router-view/>
+</template>
+
+<script>
+import Header from '@/components/header/Header.vue'
+import { getSeller } from '@/api'
+export default {
+  components: {
+    'v-header': Header
+  },
+  data (){
+      return {
+        // 存储商家信息,给子组件传值
+        sellerData: {}
+      }
+  },
+  created() {
+    getSeller().then(res => {
+      console.log(res);
+      this.sellerData = res
+    })
+  }
+}
+
+</script>
+
+<style lang="less">
+
+</style>
