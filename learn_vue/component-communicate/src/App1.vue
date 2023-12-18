dfa207fb8271e1be09eb6403b1241ca5120ddeea
9GETUP <liuyinhang0315@163.com>
2023-12-18 23:03:47 +0800

    vue-communicate

---
+++ learn_vue/component-communicate/src/App1.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="head">
+            <!-- v-model双向数据绑定 -->
+            <input type="text" v-model="message">
+            <button @click="submit">确定</button>
+        </div>
+        <!-- 父给子传值 -->
+        <List :lists="lists"/>
+        <!-- <List :msg="message"/> -->
+    </div>
+</template>
+  
+<script>
+import List from '@/components/body1/List.vue'
+export default {
+    components:{
+        List
+    },
+    data() {
+        return {
+            lists: ['html', 'css'],
+            message: '',
+        }
+    },
+    watch:{
+        lists(newVal){
+            console.log(newVal);
+        }
+    },
+    methods: {
+        submit() {
+            // console.log(this.message);
+            if (!this.message) return
+            this.lists.push(this.message)
+            this.message = ''
+        }
+    }
+}
+</script>
+  
+<style lang="css" scoped></style>
+  
---
+++ learn_vue/component-communicate/src/components/body1/List.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="body">
+        <ul>
+          <li v-for="(item,index) in lists" :key="index">{{item}}</li>
+        </ul>
+
+        <button @click="changeProps">修改props</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                // lists: ['html', 'css'],
+            }
+        },
+        // 用props接收父组件的值
+        props: ['lists'],
+        methods: {
+            changeProps(){
+                // this.lists[0] = 'HTML' 不能这样写
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
